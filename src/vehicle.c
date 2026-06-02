95efdf302ecc321f546c5620a68d24a40de04112
Lukas-Lion <lulomt00@hs-esslingen.de>
2026-06-02 20:03:30 +0200

    Neues Feature: Im Menu kann durch wählen der 1 ein Fahrzeug ausgesucht werden. Die Funktion gibt den Dateinamen für spätere Berechnungen zurück.

--- src/vehicle.c
+++ src/vehicle.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 #include "vehicle.h"
 
-void select_vehicle(void) {
+char* select_vehicle(void) {
+    int input;
+
     printf("Du hast 'Fahrzeug auswaehlen' gewaehlt.\n");
+
+    printf("Waehle zwischen folgenden Fahrzeugen:\n");
+    printf("1 - Renault Zoe\n");
+    printf("2 - Tesla Model 3\n");
+    printf("3 - VW ID.3\n");
+
+    while (1) {
+        scanf("%d", &input);
+
+        if (input == 1) {
+            printf("Du hast 'Renault Zoe' ausgewaehlt.\n");
+            return "Renault Zoe";} 
+        else if (input == 2) {
+            printf("Du hast 'Tesla Model 3' ausgewaehlt.\n");
+            return "Tesla Model 3";} 
+        else if (input == 3) {
+            printf("Du hast 'VW ID.3' ausgewaehlt.\n");
+            return "VW ID.3";} 
+        else {
+            printf("Ungueltige Eingabe. Bitte erneut versuchen.\n");}
+    }
 }
